92b5374bcf21b08aa1cf53df62575644f1bb54ff
Kenanimok <sutha.w@touchtechnologeis.co.th>
2022-11-16 11:58:01 +0700

    fix login

--- contrllers/auth.js
+++ contrllers/auth.js
@@ -35,8 +35,39 @@ exports.listUser = async (req, res) => {
   }
 };
 
+// exports.login = async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     var user = await User.findOneAndUpdate({ username }, { new: true });
+//     if (user && user.enabled) {
+//       // Check Password
+//       const isMatch = await bcrypt.compare(password, user.password);
+
+//       if (!isMatch) {
+//         return res.status(400).send("Password Invalid!!");
+//       }
+//       const payload = {
+//         user: {
+//           username: user.username,
+//           role: user.role,
+//         },
+//       };
+//       jwt.sign(payload, "jwtSecret", { expiresIn: 3600 }, (err, token) => {
+//         if (err) throw err;
+//         res.json({ token, payload });
+//       });
+//     } else {
+//       return res.status(400).send("User Not found!!!");
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send("Server Error!");
+//   }
+// };
+
 exports.login = async (req, res) => {
   try {
+    console.log(req.body);
     const { username, password } = req.body;
     var user = await User.findOneAndUpdate({ username }, { new: true });
     if (user && user.enabled) {
@@ -46,16 +77,19 @@ exports.login = async (req, res) => {
       if (!isMatch) {
         return res.status(400).send("Password Invalid!!");
       }
+      // Payload
       const payload = {
         user: {
           username: user.username,
           role: user.role,
         },
       };
+      // Generate Token
       jwt.sign(payload, "jwtSecret", { expiresIn: 3600 }, (err, token) => {
         if (err) throw err;
         res.json({ token, payload });
       });
+      // res.send("login success");
     } else {
       return res.status(400).send("User Not found!!!");
     }
